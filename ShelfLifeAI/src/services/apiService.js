73aecd48afb87a9f5b126879a1e63df0c0b59553
deshan2004 <deshandhakshitha16@gmail.com>
2026-07-09 15:03:25 +0530

    Update apiService.js

--- ShelfLifeAI/src/services/apiService.js
+++ ShelfLifeAI/src/services/apiService.js
@@ -1,5 +1,5 @@
 // src/services/apiService.js
-const API_URL = import.meta.env?.VITE_API_URL || 'http://localhost:5000';
+const API_URL = import.meta.env?.VITE_API_URL || ' https://accustom-alias-altitude.ngrok-free.dev ';
 
 export const api = {
   // ===== HEALTH =====
